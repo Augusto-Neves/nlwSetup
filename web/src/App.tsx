63b81b6d456e392f84198549de089c691e917303
Augusto Neves <augustoneves@frwk.com.br>
2023-01-17 15:04:44 -0300

    Initial Commit

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
